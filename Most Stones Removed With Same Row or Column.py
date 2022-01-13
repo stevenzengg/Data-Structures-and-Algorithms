4bd4e731a033aa0076663d32ecd0976ba83b8830
Steven Zeng <zengstevenz@gmail.com>
2022-01-13 15:19:07 -0500

    asd

---
+++ Most Stones Removed With Same Row or Column.py
@@ -0,0 +1,29 @@
+import collections
+class Solution:
+    def removeStones(self, stones):
+        def dfs(i, j):
+            points.discard((i, j))
+            for y in rows[i]:
+                if (i, y) in points:
+                    dfs(i, y)
+            for x in cols[j]:
+                if (x, j) in points:
+                    dfs(x, j)
+        points, island, rows, cols = {(i, j) for i, j in stones}, 0, collections.defaultdict(list), collections.defaultdict(list)
+        for i, j in stones:
+            rows[i].append(j)
+            cols[j].append(i)
+        for i, j in stones:
+            if (i, j) in points:
+                dfs(i, j)
+                island += 1
+        return len(stones) - island
+
+
+
+if __name__ == '__main__':
+    a = Solution()
+
+    b = [[0,0],[0,2],[1,1],[2,0],[2,2]]
+
+    print(a.removeStones(b))
